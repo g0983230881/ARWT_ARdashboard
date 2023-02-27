defacd598855700fffb6f71390f0ebe72f4e06f3
Jordan <g0983230881@gmail.com>
2023-02-27 14:28:35 +0800

    ARMarker Rotate and Scale_has_canvas
    ARMarker Rotate and Scale_has_canvas

--- README.md
+++
@@ -1,2 +0,0 @@
-# ARWT_ARdashboard
-[WebAR 測試](https://g0983230881.github.io/)
--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-const unityInstance = UnityLoader.instantiate("unityContainer", "Build/WebGL_dashboard.json");
+const unityInstance = UnityLoader.instantiate("unityContainer", "Build/WebGL_house_canvas.json");
 let isCameraReady = false;
 let isDetectionManagerReady = false;
 let gl = null;
